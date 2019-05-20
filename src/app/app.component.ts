fca269e5ffe9936beedee5a55060642931275ff9
DWS-paris <j.noyer@digitalworkshop.fr>
2019-05-20 10:25:06 +0200

    NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,43 @@ Export
     Declaration
     */
       public title: string;
+      public dataCollection: Array<any>;
 
       // Class injection
       constructor(){
         this.title = `Hello Angular`;
+        this.dataCollection = [
+          {
+            _id: "1493",
+            employee_name: "Julien",
+            employee_salary: "0",
+            employee_age: "39"
+            },
+            {
+            _id: "1494",
+            employee_name: "AL",
+            employee_salary: "2200",
+            employee_age: "25"
+            },
+            {
+            _id: "1496",
+            employee_name: "Eminem",
+            employee_salary: "2323",
+            employee_age: "34"
+            },
+            {
+            _id: "1497",
+            employee_name: "Adam 5",
+            employee_salary: "1111",
+            employee_age: "35"
+            },
+            {
+            _id: "1500",
+            employee_name: "Adam 8",
+            employee_salary: "1111",
+            employee_age: "35"
+            }
+          ]
       }
     //
 
